c838ad19bfdc927574c7821b97c906afa8866a75
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-02-07 23:35:20 +0100

    add FilePicker struct

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,32 +2,37 @@ use macroquad::prelude::*;
 use quad_files::*;
 #[macroquad::main("example :>")]
 async fn main() {
-    let mut file_data: Vec<u8> = Vec::new();
-    let mut dialog_open = false;
+    let mut file_data: Option<Vec<u8>> = None;
+    let mut dialog = FilePicker::new();
     loop {
         clear_background(BLACK);
-        if file_data.is_empty() && dialog_open {
-            let data = read_contents();
-            if let FileInputResult::Data(data) = data {
-                dialog_open = false;
-                file_data = data;
-            } else if let FileInputResult::Canceled = data {
-                dialog_open = false;
-            }
+        // update dialog, if theres data, store it in file_data
+        if let FileInputResult::Data(data) = dialog.update() {
+            file_data = Some(data);
         }
         if is_key_pressed(KeyCode::Space) {
-            open_dialog();
-            dialog_open = true;
+            dialog.open_dialog();
         }
 
         draw_text("press [space] to select a file!", 20.0, 20.0, 30.0, WHITE);
-        draw_text(
-            &format!("file data: {:?}", file_data),
-            20.0,
-            70.0,
-            30.0,
-            WHITE,
-        );
+
+        // if file data has been read
+        if let Some(file_data) = &file_data {
+            // try to parse as text
+            let as_text = std::str::from_utf8(&file_data);
+            if let Ok(text) = as_text {
+                draw_text(&format!("file text: {}", text), 20.0, 70.0, 30.0, WHITE);
+            } else {
+                // if not utf-8, display raw bytes
+                draw_text(
+                    &format!("file data: {:?}", file_data),
+                    20.0,
+                    70.0,
+                    30.0,
+                    WHITE,
+                );
+            }
+        }
         next_frame().await
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,77 @@ const NULL_SIGNATURE: [u8; 3] = [1, 48, 90];
 // signature for user cancel input
 const CANCEL_SIGNATURE: [u8; 3] = [1, 48, 91];
 
+#[derive(Clone)]
+pub enum FileInputResult {
+    Canceled,
+    None,
+    Data(Vec<u8>),
+}
+
+pub struct FilePicker {
+    waiting_for_result: bool,
+    #[cfg(not(target_arch = "wasm32"))]
+    result_buffer: FileInputResult,
+}
+
+impl FilePicker {
+    pub fn new() -> Self {
+        #[cfg(target_arch = "wasm32")]
+        {
+            FilePicker {
+                waiting_for_result: false,
+            }
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            FilePicker {
+                waiting_for_result: false,
+                result_buffer: FileInputResult::None,
+            }
+        }
+    }
+    pub fn open_dialog(&mut self) {
+        self.waiting_for_result = true;
+        #[cfg(target_arch = "wasm32")]
+        {
+            open_dialog();
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            let path = rfd::FileDialog::new().pick_file();
+            if let Some(path) = path {
+                self.result_buffer = FileInputResult::Data(std::fs::read(path).unwrap());
+            } else {
+                self.result_buffer = FileInputResult::Canceled;
+            }
+        }
+    }
+    pub fn update(&mut self) -> FileInputResult {
+        #[cfg(target_arch = "wasm32")]
+        {
+            if self.waiting_for_result {
+                let contents = read_contents();
+                if !matches!(contents, FileInputResult::None) {
+                    self.waiting_for_result = false;
+                }
+                contents
+            } else {
+                FileInputResult::None
+            }
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            if self.waiting_for_result {
+                self.waiting_for_result = false;
+                self.result_buffer.clone()
+            } else {
+                FileInputResult::None
+            }
+        }
+    }
+}
+
+/// Opens a dialog. Contents can be read by read_contents(). Consider using a [FilePicker] instead, as they handle opnening the dialog and awaiting the result automatically.
 pub fn open_dialog() {
     #[cfg(target_arch = "wasm32")]
     {
@@ -33,12 +104,7 @@ pub fn open_dialog() {
     }
 }
 
-pub enum FileInputResult {
-    Canceled,
-    None,
-    Data(Vec<u8>),
-}
-
+/// Reads content from previously inputted file. Consider using a [FilePicker] instead, as they handle opnening the dialog and awaiting the result automatically.
 pub fn read_contents() -> FileInputResult {
     #[cfg(target_arch = "wasm32")]
     {
@@ -52,5 +118,5 @@ pub fn read_contents() -> FileInputResult {
         }
         return FileInputResult::Data(buf);
     }
-    FileInputResult::Canceled;
+    FileInputResult::Canceled
 }
